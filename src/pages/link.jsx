602226a744c163bc13b1f6c8e93a1aafa2fde965
5AIPAVAN <123063767+5AIPAVAN@users.noreply.github.com>
2024-12-26 22:42:03 +0530

    domain updated

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -92,9 +92,9 @@ const Link = () => {
             {url?.title}
           </span>
 
-          <a href={`http://localhost:5173/${link}`} target='_blank'
+          <a href={`https://trimmrrr.vercel.app/${link}`} target='_blank'
             className='text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer'>
-            http://localhost:5173/{link}
+            https://trimmrrr.vercel.app/{link}
           </a>
 
           <a href={url?.original_url} target='_blank'
